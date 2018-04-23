f7c7d750ae2c100de8cb3b7d0e8e32e08d5f5f30
khandagalekrishna8@gmail.com <khandagalekrishna8@gmail.com>
2018-04-23 11:04:29 +0530

    Base Cativity created

---
+++ project/app/src/main/java/MainTabsActivity.java
@@ -0,0 +1,78 @@
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+
+import com.electivechaos.checklist.R;
+import com.electivechaos.checklist.fragments.CategoryListFragment;
+import com.electivechaos.checklist.fragments.ReportListFragment;
+
+public class MainTabsActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_tabs_layout_activity);
+
+        Toolbar toolbar =  findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ViewPager viewPager = findViewById(R.id.main_viewpager);
+
+        MainTabsActivityPagerAdapter mainTabsActivityPagerAdapter = new MainTabsActivityPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(mainTabsActivityPagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+
+
+
+
+
+
+
+
+    //View pager  for showing two tabs in the welcome activity
+
+    public class MainTabsActivityPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MainTabsActivityPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            if( i ==0 ){
+                Fragment fragment = new ReportListFragment();
+                return fragment;
+            }else{
+                Fragment fragment = new CategoryListFragment();
+                return fragment;
+            }
+
+        }
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if(position == 0){
+                return  "Report List";
+            }else {
+                return  "Categories";
+            }
+        }
+    }
+
+
+
+
+
+}
