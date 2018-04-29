52c35c050a784f00a9f7f1309bece046996b15c8
khandagalekrishna8@gmail.com <khandagalekrishna8@gmail.com>
2018-04-30 01:24:36 +0530

    Category UI validation IN PROGRESS.

--- project/app/src/main/java/com/electivechaos/checklistapp/ui/AddEditCategoryActivity.java
+++ project/app/src/main/java/com/electivechaos/checklistapp/ui/AddEditCategoryActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.electivechaos.checklistapp.R;
+import com.electivechaos.checklistapp.utils.CommonUtils;
 
 /**
  * Created by barkhasikka on 25/04/18.
@@ -19,49 +20,64 @@ public class AddEditCategoryActivity extends AppCompatActivity{
     private  String intentCategoryTitle = null;
     private  String intentCategoryDescription = null;
     private  int categoryID = -1;
+
+    private  EditText categoryName;
+    private EditText categoryDescription;
+    private View addEditCategoryParentLayout;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Get the view from new_activity.xml
         setContentView(R.layout.add_edit_category);
-
+        categoryName = findViewById(R.id.editTextCategoryName);
+        categoryDescription = findViewById(R.id.editTextCategoryDescription);
+        addEditCategoryParentLayout = findViewById(R.id.addEditCategoryParentLayout);
         if(getIntent().getExtras()!= null){
-            Bundle dataFromActivity = getIntent().getBundleExtra("data");
+            Bundle dataFromActivity = getIntent().getBundleExtra(
+                    "data");
             intentCategoryTitle = dataFromActivity.get("title").toString();
             intentCategoryDescription = dataFromActivity.get("description").toString();
             categoryID = Integer.parseInt(dataFromActivity.get("id").toString());
 
             Button updateCategoryButton = findViewById(R.id.updateCategory);
-            updateCategoryButton.setText("Update Category");
+            updateCategoryButton.setText(R.string.update_category);
 
-            EditText categoryName = findViewById(R.id.editTextCategoryName);
+
             categoryName.setText(intentCategoryTitle.toString());
 
-            EditText categoryDescription = findViewById(R.id.editTextCategoryDescription);
             categoryDescription.setText(intentCategoryDescription.toString());
         }
 
         Button updateCategoryButton = findViewById(R.id.updateCategory);
         updateCategoryButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                EditText categoryName = findViewById(R.id.editTextCategoryName);
-                String categoryNameString = categoryName.getText().toString();
+                String categoryNameString = categoryName.getText().toString().trim();
 
-                EditText categoryDescription = findViewById(R.id.editTextCategoryDescription);
-                String categoryDescriptionString = categoryDescription.getText().toString();
+
+                String categoryDescriptionString = categoryDescription.getText().toString().trim();
+
+                if(categoryNameString.isEmpty()){
+                    CommonUtils.hideKeyboard(AddEditCategoryActivity.this);
+                    CommonUtils.showSnackbarMessage("Please enter category name.", true, true,addEditCategoryParentLayout,AddEditCategoryActivity.this);
+                    return;
+                }else if(categoryDescriptionString.isEmpty()){
+                    CommonUtils.hideKeyboard(AddEditCategoryActivity.this);
+                    CommonUtils.showSnackbarMessage("Please enter category description.", true, true,addEditCategoryParentLayout,AddEditCategoryActivity.this);
+                    return;
+                }
+
                 Bundle data = new Bundle();//create bundle instance
                 if(categoryID == -1) {
-                    data.putString("categoryName", categoryNameString);//put string to pass with a key value
-                    data.putString("categoryDesc", categoryDescriptionString);//put string to pass with a key value
+                    data.putString("categoryName", categoryNameString);
+                    data.putString("categoryDesc", categoryDescriptionString);
                     Intent intent = new Intent();
                     intent.putExtra("data", data);
                     setResult(Activity.RESULT_OK, intent);
                     finish();
                 }else {
                     Intent intent = new Intent();
-                    data.putString("categoryName", categoryNameString);//put string to pass with a key value
-                    data.putString("categoryDesc", categoryDescriptionString);//put string to pass with a key value
-                    data.putInt("categoryId", categoryID);//put string to pass with a key value
+                    data.putString("categoryName", categoryNameString);
+                    data.putString("categoryDesc", categoryDescriptionString);
+                    data.putInt("categoryId", categoryID);
                     intent.putExtra("data", data);
                     setResult(Activity.RESULT_OK, intent);
                     finish();
