220761e87e6c15ea3f52d6855a8063d4b7f8fb65
khandagalekrishna8@gmail.com <khandagalekrishna8@gmail.com>
2018-04-29 11:04:47 +0530

    Google map integgration work is in progress.

--- project/app/src/main/java/com/electivechaos/checklistapp/fragments/LossLocationFragment.java
+++ project/app/src/main/java/com/electivechaos/checklistapp/fragments/LossLocationFragment.java
@@ -1,5 +1,6 @@
 package com.electivechaos.checklistapp.fragments;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
@@ -52,32 +53,37 @@ import static android.content.ContentValues.TAG;
  * Created by krishna on 2/26/18.
  */
 
-public class LossLocationFragment extends Fragment  implements  GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+public class LossLocationFragment extends Fragment implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
     MapView mMapView;
     private GoogleMap googleMap;
+
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+    private static final int GOOGLE_API_CLIENT_ID = 0;
+
+
     private boolean mLocationPermissionGranted;
     private PlaceDetectionClient mPlaceDetectionClient;
     private final LatLng mDefaultLocation = new LatLng(-33.8523341, 151.2106085);
 
 
-    private static final int GOOGLE_API_CLIENT_ID = 0;
     private GoogleApiClient mGoogleApiClient;
     private PlaceArrayAdapter mPlaceArrayAdapter;
     private AutoCompleteTextView mAutocompleteTextView;
     private static final LatLngBounds BOUNDS_MOUNTAIN_VIEW = new LatLngBounds(new LatLng(37.398160, -122.180831), new LatLng(37.430610, -121.972090));
 
-    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.loss_location_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.loss_location_fragment, container, false);
         mMapView = rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
 
-        mMapView.onResume(); // needed to get the map to display immediately
+        mMapView.onResume();
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
@@ -100,11 +106,14 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
             }
         });
 
-        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-                .addApi(Places.GEO_DATA_API)
-                .enableAutoManage(getActivity(), GOOGLE_API_CLIENT_ID, this)
-                .addConnectionCallbacks(this)
-                .build();
+        if (mGoogleApiClient == null){
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                    .addApi(Places.GEO_DATA_API)
+                    .enableAutoManage(getActivity(), GOOGLE_API_CLIENT_ID, this)
+                    .addConnectionCallbacks(this)
+                    .build();
+        }
+
         mAutocompleteTextView = rootView.findViewById(R.id.place_autocomplete_text_view);
         mAutocompleteTextView.setThreshold(2);
         mAutocompleteTextView.setOnItemClickListener(mAutocompleteClickListener);
@@ -113,18 +122,17 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
 
         mPlaceDetectionClient = Places.getPlaceDetectionClient(getActivity(), null);
         displayLocationSettingsRequest(getActivity());
-        return  rootView;
+        return rootView;
     }
+
     private AdapterView.OnItemClickListener mAutocompleteClickListener = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             final PlaceArrayAdapter.PlaceAutocomplete item = mPlaceArrayAdapter.getItem(position);
             final String placeId = String.valueOf(item.placeId);
-            Log.i("Location", "Selected: " + item.fulltext);
             PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
                     .getPlaceById(mGoogleApiClient, placeId);
             placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
-            Log.i("Location", "Fetching details for ID: " + item.placeId);
         }
     };
 
@@ -133,15 +141,9 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
         @Override
         public void onResult(PlaceBuffer places) {
             if (!places.getStatus().isSuccess()) {
-                Log.e("Location", "Place query did not complete. Error: " +
-                        places.getStatus().toString());
                 return;
             }
-            // Selecting the first object buffer.
             final Place place = places.get(0);
-
-            Log.i("name", place.getName().toString());
-            Log.i("coordinates", place.getLatLng().toString());
         }
     };
 
@@ -150,16 +152,11 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
      * Prompts the user for permission to use the device location.
      */
     private void getLocationPermission() {
-        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             mLocationPermissionGranted = true;
-//            updateLocationUI();
             showCurrentPlace();
         } else {
-            ActivityCompat.requestPermissions(getActivity(),
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
         }
     }
 
@@ -170,9 +167,7 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
         mLocationPermissionGranted = false;
         switch (requestCode) {
             case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     mLocationPermissionGranted = true;
                     showCurrentPlace();
                 }
@@ -200,10 +195,11 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
                 googleMap.getUiSettings().setMyLocationButtonEnabled(false);
                 getLocationPermission();
             }
-        } catch (SecurityException e)  {
+        } catch (SecurityException e) {
             Log.e("Exception: %s", e.getMessage());
         }
     }
+
     @Override
     public void onResume() {
         super.onResume();
@@ -236,9 +232,10 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
         }
 
         if (mLocationPermissionGranted) {
-            // Get the likely places - that is, the businesses and other points of interest that
-            // are the best match for the device's current location.
-            @SuppressWarnings("MissingPermission") final Task<PlaceLikelihoodBufferResponse> placeResult =
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            final Task<PlaceLikelihoodBufferResponse> placeResult =
                     mPlaceDetectionClient.getCurrentPlace(null);
             placeResult.addOnCompleteListener
                     (new OnCompleteListener<PlaceLikelihoodBufferResponse>() {
@@ -250,9 +247,9 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
                                 LatLng sydney = ltlng;
                                 googleMap.addMarker(new MarkerOptions().position(sydney).title("Your Location").snippet(likelyPlaces.get(0).getPlace().getAddress().toString()));
 
-                                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(8).build();
                                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-                                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+                                googleMap.getUiSettings().setMyLocationButtonEnabled(false);
                                 likelyPlaces.release();
                             } else {
                                 Log.e(TAG, "Exception: %s", task.getException());
@@ -265,7 +262,6 @@ public class LossLocationFragment extends Fragment  implements  GoogleApiClient.
                     .position(mDefaultLocation)
                     .snippet("No"));
 
-            // Prompt the user for permission.
             getLocationPermission();
         }
     }
