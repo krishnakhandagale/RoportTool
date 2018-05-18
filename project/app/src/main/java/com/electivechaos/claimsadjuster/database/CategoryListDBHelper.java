b506566b186b0a7707f9de1edd26e00334723064
Unknown <naffu.aziz@gmail.com>
2018-05-18 12:50:46 +0530

    Updated database.

--- project/app/src/main/java/com/electivechaos/claimsadjuster/database/CategoryListDBHelper.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/database/CategoryListDBHelper.java
@@ -23,7 +23,7 @@ import java.util.Iterator;
  */
 
 public class CategoryListDBHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 57;
+    private static final int DATABASE_VERSION = 58;
 
 
     // Database Name
