b253e39c9bae5682b5456d9f86305c4dc09454a6
khandagalekrishna8@gmail.com <khandagalekrishna8@gmail.com>
2018-05-23 10:11:09 +0530

    Customisation work in progress.

--- project/app/src/main/java/com/electivechaos/claimsadjuster/ui/CustomisationActivity.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/ui/CustomisationActivity.java
@@ -1,10 +1,22 @@
 package com.electivechaos.claimsadjuster.ui;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.electivechaos.claimsadjuster.R;
+import com.electivechaos.claimsadjuster.pojo.Label;
+
+import java.util.ArrayList;
 
 public class CustomisationActivity extends AppCompatActivity {
 
@@ -12,5 +24,60 @@ public class CustomisationActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customise_report_activity);
+
+
+        RecyclerView recyclerView = findViewById(R.id.customLabelPositionView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        ArrayList<Label> labels = new ArrayList<>();
+        labels.add(new Label());
+        labels.add(new Label());
+        labels.add(new Label());
+
+        Adapter adapter = new Adapter(this,labels);
+        recyclerView.setAdapter(adapter);
+
+
     }
+
+
+
+    private class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
+        Context context;
+        ArrayList<Label> labelArrayList;
+        Adapter(Context context, ArrayList<Label> labelArrayList){
+            this.context = context;
+            this.labelArrayList = labelArrayList;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater =  LayoutInflater.from(context);
+            View view = inflater.inflate(R.layout.label_rearragement_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.label.setText(labelArrayList.get(position).getName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return labelArrayList.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView label;
+            public ViewHolder(View itemView) {
+                super(itemView);
+                label = itemView.findViewById(R.id.label);
+            }
+        }
+    }
+
+
 }
