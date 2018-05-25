66429c1f96c2f9e4b4fc89400be04f6e0a294502
Unknown <naffu.aziz@gmail.com>
2018-05-25 16:58:38 +0530

    Code clean up.

--- project/app/src/main/java/com/electivechaos/claimsadjuster/ui/CustomisationActivity.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/ui/CustomisationActivity.java
@@ -1,7 +1,6 @@
 package com.electivechaos.claimsadjuster.ui;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
