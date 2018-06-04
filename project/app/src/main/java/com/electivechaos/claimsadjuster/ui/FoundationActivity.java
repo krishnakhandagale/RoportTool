51bab35120145b0e3b6260970f6dc62254d4cfb5
Unknown <naffu.aziz@gmail.com>
2018-06-04 14:30:04 +0530

    Foundation PopUp menu code changes.

--- project/app/src/main/java/com/electivechaos/claimsadjuster/ui/FoundationActivity.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/ui/FoundationActivity.java
@@ -7,16 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.electivechaos.claimsadjuster.R;
+import com.electivechaos.claimsadjuster.database.CategoryListDBHelper;
+import com.electivechaos.claimsadjuster.pojo.FoundationPOJO;
 import com.electivechaos.claimsadjuster.utils.CommonUtils;
 
 public class FoundationActivity extends AppCompatActivity {
+
+    private static final int UNIQUE_CONSTRAINT_FAIL_ERROR_CODE = -111;
+    private CategoryListDBHelper categoryListDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foundation);
+
+        categoryListDBHelper = CategoryListDBHelper.getInstance(this);
+
         Button addFoundationBtn = findViewById(R.id.addFoundation);
         final EditText foundationName = findViewById(R.id.foundationName);
         final View parentLayoutForMessages = findViewById(R.id.parentLayout);
@@ -29,12 +38,21 @@ public class FoundationActivity extends AppCompatActivity {
                 {
                     CommonUtils.showSnackbarMessage(getString(R.string.please_enter_name), true, true, parentLayoutForMessages, FoundationActivity.this);
                 }else {
-                    Bundle data = new Bundle();
-                    data.putString("foundationName", foundationName.getText().toString());
-                    Intent intent = new Intent();
-                    intent.putExtra("foundationDetails", data);
-                    setResult(Activity.RESULT_OK, intent);
-                    finish();
+                    FoundationPOJO foundationPOJO = new FoundationPOJO();
+                    foundationPOJO.setName(foundationName.getText().toString());
+
+                    long foundationId = categoryListDBHelper.addFoundation(foundationPOJO);
+
+                    if (foundationId == UNIQUE_CONSTRAINT_FAIL_ERROR_CODE) {
+                        Toast.makeText(FoundationActivity.this, "Foundation with same name already exists.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Bundle data = new Bundle();
+                        data.putString("foundationName", foundationName.getText().toString());
+                        Intent intent = new Intent();
+                        intent.putExtra("foundationDetails", data);
+                        setResult(Activity.RESULT_OK, intent);
+                        finish();
+                    }
                 }
             }
         });
