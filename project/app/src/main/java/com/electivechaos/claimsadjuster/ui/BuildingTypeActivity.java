5455e89e22e203f8b144cb6bc86fd7946667a0a9
Unknown <naffu.aziz@gmail.com>
2018-06-04 14:31:17 +0530

    Building Type PopUp menu code changes.

--- project/app/src/main/java/com/electivechaos/claimsadjuster/ui/BuildingTypeActivity.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/ui/BuildingTypeActivity.java
@@ -7,16 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.electivechaos.claimsadjuster.R;
+import com.electivechaos.claimsadjuster.database.CategoryListDBHelper;
+import com.electivechaos.claimsadjuster.pojo.BuildingTypePOJO;
 import com.electivechaos.claimsadjuster.utils.CommonUtils;
 
 public class BuildingTypeActivity extends AppCompatActivity {
+
+    private static final int UNIQUE_CONSTRAINT_FAIL_ERROR_CODE = -111;
+    private CategoryListDBHelper categoryListDBHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_building_type);
+
+        categoryListDBHelper = CategoryListDBHelper.getInstance(this);
+
         Button addBuildingBtn = findViewById(R.id.addBuilding);
         final EditText buildingName = findViewById(R.id.buildingName);
         final View parentLayoutForMessages = findViewById(R.id.parentLayout);
@@ -29,12 +38,22 @@ public class BuildingTypeActivity extends AppCompatActivity {
                 {
                     CommonUtils.showSnackbarMessage(getString(R.string.please_enter_name), true, true, parentLayoutForMessages, BuildingTypeActivity.this);
                 }else {
-                    Bundle data = new Bundle();
-                    data.putString("buildingName", buildingName.getText().toString());
-                    Intent intent = new Intent();
-                    intent.putExtra("buildingDetails", data);
-                    setResult(Activity.RESULT_OK, intent);
-                    finish();
+
+                    BuildingTypePOJO buildingTypePOJO = new BuildingTypePOJO();
+                    buildingTypePOJO.setName(buildingName.getText().toString());
+
+                    long buildingTypeId = categoryListDBHelper.addBuildingType(buildingTypePOJO);
+                    if (buildingTypeId == UNIQUE_CONSTRAINT_FAIL_ERROR_CODE) {
+                        Toast.makeText(BuildingTypeActivity.this, "Building type with same name already exists.", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Bundle data = new Bundle();
+                        data.putString("buildingName", buildingName.getText().toString());
+                        Intent intent = new Intent();
+                        intent.putExtra("buildingDetails", data);
+                        setResult(Activity.RESULT_OK, intent);
+                        finish();
+                    }
                 }
             }
         });
