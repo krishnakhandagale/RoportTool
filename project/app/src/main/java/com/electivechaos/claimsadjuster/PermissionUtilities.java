8e467a7942a521c1362a89cd1072f85982090437
khandagalekrishna8@gmail.com <khandagalekrishna8@gmail.com>
2018-06-15 18:59:11 +0530

    House number changes

--- project/app/src/main/java/com/electivechaos/claimsadjuster/PermissionUtilities.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/PermissionUtilities.java
@@ -26,7 +26,9 @@ public class PermissionUtilities {
     public static final int MY_APP_TAKE_FRONT_PHOTO_PERMISSIONS = 129;
     public static final int MY_APP_TAKE_BACK_PHOTO_PERMISSIONS = 130;
     public static final int MY_APP_TAKE_LEFT_PHOTO_PERMISSIONS = 131;
-    public static final int MY_APP_TAKE_RIGHT_PHOTO_PERMISSIONS = 132;
+    public static final int MY_APP_TAKE_RIGHT_PHOTO_PERMISSIONS = 135;
+
+    public static final int MY_APP_TAKE_HOUSE_NUMBER_PHOTO_PERMISSIONS = 132;
 
     public static final int MY_APP_GENERATE_REPORT_PERMISSIONS_FOUR = 133;
     public static final int MY_APP_SAVE_SNAPSHOT_PERMISSIONS = 134;
@@ -36,7 +38,7 @@ public class PermissionUtilities {
     public static boolean checkPermission(final Context context, final Fragment
             fragment, final int type) {
         int currentAPIVersion = Build.VERSION.SDK_INT;
-        if (type == MY_APP_TAKE_PHOTO_PERMISSIONS || type == MY_APP_TAKE_FRONT_PHOTO_PERMISSIONS || type == MY_APP_TAKE_BACK_PHOTO_PERMISSIONS || type == MY_APP_TAKE_LEFT_PHOTO_PERMISSIONS || type == MY_APP_TAKE_RIGHT_PHOTO_PERMISSIONS) {
+        if (type == MY_APP_TAKE_PHOTO_PERMISSIONS || type == MY_APP_TAKE_FRONT_PHOTO_PERMISSIONS || type == MY_APP_TAKE_BACK_PHOTO_PERMISSIONS || type == MY_APP_TAKE_LEFT_PHOTO_PERMISSIONS || type == MY_APP_TAKE_RIGHT_PHOTO_PERMISSIONS || type == MY_APP_TAKE_HOUSE_NUMBER_PHOTO_PERMISSIONS) {
             if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
                 if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                     if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.READ_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.WRITE_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, Manifest.permission.CAMERA)) {
