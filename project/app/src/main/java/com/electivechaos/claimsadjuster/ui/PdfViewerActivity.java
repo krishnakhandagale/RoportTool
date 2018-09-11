3e6d1400383d27ee2940a3221aaed39c9f4beb47
Unknown <naffu.aziz@gmail.com>
2018-09-11 10:31:51 +0530

    Added scrollview for setting activity.

--- project/app/src/main/java/com/electivechaos/claimsadjuster/ui/PdfViewerActivity.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/ui/PdfViewerActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -23,7 +25,6 @@ public class PdfViewerActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pdf_viewer_layout);
-
         ImageView shareBottomButton = findViewById(R.id.shareBottomButton);
 
         final PDFView pdfView = findViewById(R.id.pdfView);
@@ -58,5 +59,14 @@ public class PdfViewerActivity extends AppCompatActivity {
 
             }
         }).load();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
