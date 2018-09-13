2f146d89c3794fc4db9b983b67a72791931ee0e9
khandagalekrishna8@gmail.com <khandagalekrishna8@gmail.com>
2018-09-13 13:20:09 +0530

    Constants for report details.

---
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/Constants.java
@@ -0,0 +1,9 @@
+package com.electivechaos.claimsadjuster;
+
+public interface Constants {
+
+    int LABEL_FONT_SIZE = 12;
+    int LABEL_FONT_VALUE_SIZE = 10;
+    int HEADER_FONT_SIZE = 12;
+    int FOOTER_FONT_SIZE = 9;
+}
