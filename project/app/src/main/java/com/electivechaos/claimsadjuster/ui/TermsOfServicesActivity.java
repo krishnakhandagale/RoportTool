6d1ef9542e42579508c13e7e3b5b12d22cee79c1
Unknown <naffu.aziz@gmail.com>
2018-10-29 14:42:15 +0530

    Code changes

--- project/app/src/main/java/com/electivechaos/claimsadjuster/ui/TermsOfServicesActivity.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/ui/TermsOfServicesActivity.java
@@ -2,11 +2,13 @@ package com.electivechaos.claimsadjuster.ui;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -14,11 +16,6 @@ import android.widget.TextView;
 
 import com.electivechaos.claimsadjuster.R;
 import com.electivechaos.claimsadjuster.utils.CommonUtils;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class TermsOfServicesActivity extends AppCompatActivity {
 
@@ -46,22 +43,15 @@ public class TermsOfServicesActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
 
         TextView txtTos = findViewById(R.id.txtTos);
-        toolbar.setTitle("Terms Of Services");
+        toolbar.setTitle("Terms Of Service");
 
-        //TODO:: Uncomment after confirmation
 
-        InputStream is = getResources().openRawResource(R.raw.tos);
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        String line;
-        String entireFile = "";
-        try {
-            while((line = br.readLine()) != null) { // <--------- place readLine() inside loop
-                entireFile += (line + "\n"); // <---------- add each line to entireFile
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        String s = getString(R.string.tos_string);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            txtTos.setText(Html.fromHtml(s, Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            txtTos.setText(Html.fromHtml(s));
         }
-        txtTos.setText(entireFile); // <------- assign entireFile to TextView
 
 
         acceptBtn.setOnClickListener(new View.OnClickListener() {
