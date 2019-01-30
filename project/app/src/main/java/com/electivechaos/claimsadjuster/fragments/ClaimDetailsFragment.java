baa2c5b5524920ebf903e08f1a317168d97d044e
Unknown <naffu.aziz@gmail.com>
2019-01-30 15:56:30 +0530

    code changes.

--- project/app/src/main/java/com/electivechaos/claimsadjuster/fragments/ClaimDetailsFragment.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/fragments/ClaimDetailsFragment.java
@@ -1,22 +1,17 @@
 package com.electivechaos.claimsadjuster.fragments;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -24,34 +19,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
-import com.electivechaos.claimsadjuster.CameraActivity;
-import com.electivechaos.claimsadjuster.ImageHelper;
 import com.electivechaos.claimsadjuster.R;
-import com.electivechaos.claimsadjuster.SingleMediaScanner;
 import com.electivechaos.claimsadjuster.adapters.DrawerMenuListAdapter;
-import com.electivechaos.claimsadjuster.asynctasks.DBSelectedImagesListTsk;
-import com.electivechaos.claimsadjuster.database.CategoryListDBHelper;
-import com.electivechaos.claimsadjuster.interfaces.AsyncTaskStatusCallback;
 import com.electivechaos.claimsadjuster.interfaces.BackButtonClickListener;
 import com.electivechaos.claimsadjuster.interfaces.NextButtonClickListener;
 import com.electivechaos.claimsadjuster.interfaces.OnGenerateReportClickListener;
 import com.electivechaos.claimsadjuster.interfaces.OnSaveReportClickListener;
-import com.electivechaos.claimsadjuster.listeners.OnMediaScannerListener;
-import com.electivechaos.claimsadjuster.pojo.Category;
-import com.electivechaos.claimsadjuster.pojo.ImageDetailsPOJO;
+import com.electivechaos.claimsadjuster.interfaces.QuickCaptureListener;
+
 import com.electivechaos.claimsadjuster.pojo.Label;
-import com.electivechaos.claimsadjuster.ui.QuickImageDetailsActivity;
-import com.electivechaos.claimsadjuster.ui.SingleImageDetailsActivity;
-import com.electivechaos.claimsadjuster.utils.ReportConstants;
-import com.electivechaos.claimsadjuster.interfaces.SelectedImagesDataInterface;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 
 public class ClaimDetailsFragment extends Fragment {
 
@@ -85,16 +64,9 @@ public class ClaimDetailsFragment extends Fragment {
     private BackButtonClickListener backButtonClickListener;
 
     private FloatingActionButton selectPhoto;
-    private String mCurrentPhotoPath;
-    private Uri fileUri;
-    private File photoFile;
-    private CoordinatorLayout parentLayout;
-    private CategoryListDBHelper categoryListDBHelper;
-    private SelectedImagesDataInterface selectedImagesDataInterface;
-    private int labelPosition;
-    private String labelDefaultCoverageType;
-    private Label label;
-
+//    private String mCurrentPhotoPath;
+//    private File photoFile;
+    private QuickCaptureListener quickCaptureListener;
 
 
 
@@ -120,27 +92,6 @@ public class ClaimDetailsFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-//            labelPosition = (int) getArguments().get("position");
-            label = getArguments().getParcelable("label");
-            labelDefaultCoverageType = (String) getArguments().get("labelDefaultCoverageType");
-
-            mCurrentPhotoPath = getArguments().getString("fileUri");
-            if (mCurrentPhotoPath != null) {
-                photoFile = new File(mCurrentPhotoPath);
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-                    fileUri = Uri.fromFile(photoFile);
-
-                } else {
-                    fileUri = FileProvider.getUriForFile(getContext().getApplicationContext(),
-                            getContext().getApplicationContext().getPackageName() + ".fileprovider",
-                            photoFile);
-                }
-
-            }
-
-        }
-
     }
 
     @Override
@@ -175,14 +126,12 @@ public class ClaimDetailsFragment extends Fragment {
 
 
         selectPhoto = view.findViewById(R.id.btnSelectPhoto);
-        parentLayout = view.findViewById(R.id.parentLayout);
-        categoryListDBHelper = CategoryListDBHelper.getInstance(getContext());
 
 
         selectPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cameraIntent(250);
+                quickCaptureListener.onClickCapture();
             }
         });
 
@@ -232,7 +181,6 @@ public class ClaimDetailsFragment extends Fragment {
             }
         });
 
-        //Add code
         fabGoBackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -298,7 +246,7 @@ public class ClaimDetailsFragment extends Fragment {
             onLabelAddClickListener = (DrawerMenuListAdapter.OnLabelAddClickListener) getActivity();
             onSaveReportClickListener = (OnSaveReportClickListener) getActivity();
             onGenerateReportClickListener = (OnGenerateReportClickListener) getActivity();
-            selectedImagesDataInterface = (SelectedImagesDataInterface) getActivity();
+            quickCaptureListener = (QuickCaptureListener) getActivity();
 
 
         } catch (ClassCastException ex) {
@@ -355,147 +303,6 @@ public class ClaimDetailsFragment extends Fragment {
             }
         }
     }
-
-
-
-    private void cameraIntent(int requestId) {
-
-        Intent intent = new Intent(getContext(), CameraActivity.class);
-        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
-
-            photoFile = getOutputMediaFile();
-            if (photoFile != null) {
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-                    fileUri = Uri.fromFile(photoFile);
-
-                } else {
-                    fileUri = FileProvider.getUriForFile(getContext().getApplicationContext(),
-                            getContext().getApplicationContext().getPackageName() + ".fileprovider",
-                            photoFile);
-                }
-
-
-                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, mCurrentPhotoPath);
-                ImageHelper.grantAppPermission(getContext(), intent, fileUri);
-//                onSetImageFileUriListener.onSetImageFileUri(mCurrentPhotoPath);
-                getActivity().startActivityForResult(intent, requestId);
-            }
-        }
-    }
-
-            private File getOutputMediaFile() {
-                File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                        Environment.DIRECTORY_PICTURES), "ReportClickedImages");
-
-                if (!mediaStorageDir.exists()) {
-                    if (!mediaStorageDir.mkdirs()) {
-                        return null;
-                    }
-                }
-
-                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-                File imageFile = new File(mediaStorageDir.getPath() + File.separator +
-                        "IMG_" + timeStamp + ".jpg");
-
-                mCurrentPhotoPath = imageFile.getAbsolutePath();
-                return imageFile;
-            }
-
-    public void onImageCapturedResult(Intent data) {
-        if (fileUri != null) {
-            onCaptureImageResult(data);
-        } else {
-            Snackbar snackbar = Snackbar
-                    .make(parentLayout, "Something went wrong.Please retry with system camera app.", Snackbar.LENGTH_INDEFINITE);
-            snackbar.setActionTextColor(ContextCompat.getColor(getActivity(), R.color.white_color));
-            snackbar.setAction("RETRY", new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-//                    selectImage(ReportConstants.REQUEST_CAMERA, ReportConstants.SELECT_FILE);
-                    v.setVisibility(View.GONE);
-                }
-            });
-            View snackBarView = snackbar.getView();
-            snackBarView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.color_error));
-            snackbar.show();
-        }
-    }
-
-
-    private void onCaptureImageResult(Intent data) {
-        new SingleMediaScanner(getContext(), photoFile, new OnMediaScannerListener() {
-            @Override
-            public void onMediaScanComplete(String path, Uri uri) {
-                if (path != null) {
-                    path = mCurrentPhotoPath;
-                    ImageDetailsPOJO imgObj = new ImageDetailsPOJO();
-                    imgObj.setDescription("");
-                    imgObj.setTitle("");
-                    imgObj.setIsDamage(false);
-                    imgObj.setOverview(false);
-                    imgObj.setPointOfOrigin(false);
-                    imgObj.setImageUrl(path);
-                    imgObj.setCoverageTye(labelDefaultCoverageType);
-
-                    File file = new File(path);
-                    if (file.exists()) {
-                        imgObj.setImageName(file.getName());
-                        Date date = new Date(file.lastModified());
-                        String dateString = new SimpleDateFormat("dd/MM/yyyy").format(date);
-                        String timeString = new SimpleDateFormat("HH:mm:ss a").format(date);
-                        imgObj.setImageDateTime(dateString + " at " + timeString);
-                        imgObj.setImageGeoTag("");
-                    }
-                    final ArrayList<ImageDetailsPOJO> capturedImage = new ArrayList<ImageDetailsPOJO>();
-                    capturedImage.add(imgObj);
-
-                   // selectedImagesDataInterface.setCapturedImage(capturedImage, 1);
-
-                    ArrayList<ImageDetailsPOJO> returnedImageItem = (ArrayList<ImageDetailsPOJO>) capturedImage;
-                    Intent intent = new Intent(getActivity(), QuickImageDetailsActivity.class);
-                    if (returnedImageItem != null) {
-                        intent.putExtra("image_details", returnedImageItem.get(0));
-                    }
-                    intent.putExtra("labelPosition", labelPosition);
-                    intent.putExtra("labelDefaultCoverageType", labelDefaultCoverageType);
-                    getActivity().startActivityForResult(intent, ReportConstants.SET_QUICK_CLICKED_CAPTURED_DETAILS);
-
-//                    new DBSelectedImagesListTsk(categoryListDBHelper, "quick_captured_image", label, capturedImage, new AsyncTaskStatusCallback() {
-//
-//                        @Override
-//                        public void onPostExecute(Object object, String type) {
-//
-//                            ImageHelper.revokeAppPermission(getActivity(), fileUri);
-//
-//                            ArrayList<ImageDetailsPOJO> returnedImageItem = (ArrayList<ImageDetailsPOJO>) object;
-//                            Intent intent = new Intent(getActivity(), QuickImageDetailsActivity.class);
-//                            if (returnedImageItem != null) {
-//                                intent.putExtra("image_details", returnedImageItem.get(0));
-//                            }
-//                            intent.putExtra("labelPosition", labelPosition);
-//                            intent.putExtra("labelDefaultCoverageType", labelDefaultCoverageType);
-//                            getActivity().startActivityForResult(intent, ReportConstants.SET_CLICKED_CAPTURED_DETAILS);
-//
-//                        }
-//
-//                        @Override
-//                        public void onPreExecute() {
-//
-//                        }
-//
-//                        @Override
-//                        public void onProgress(int progress) {
-//
-//                        }
-//                    }).execute();
-
-
-                }
-            }
-        });
-    }
-
 
 }
 
