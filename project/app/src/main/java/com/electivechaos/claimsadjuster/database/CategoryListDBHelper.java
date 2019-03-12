6ec150f952deea4c27f45805485f1a018d1eb88a
Unknown <naffu.aziz@gmail.com>
2019-03-12 18:45:37 +0530

    db version changed.

--- project/app/src/main/java/com/electivechaos/claimsadjuster/database/CategoryListDBHelper.java
+++ project/app/src/main/java/com/electivechaos/claimsadjuster/database/CategoryListDBHelper.java
@@ -29,7 +29,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class CategoryListDBHelper extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 203;
+    private static final int DATABASE_VERSION = 204;
 
     // Database Name
     private static final String DATABASE_NAME = "master_categories_list";
